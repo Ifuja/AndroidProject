76ac00fcb6ea18b4bf363feac2febd82eef113d9
ifuja <ifuja.chamoinri@gmail.com>
2018-05-18 13:40:55 +0200

    Android Project 1

---
+++ app/src/main/java/org/esiea/chamoinri_ifuja_firmin_jacques_olivier/androidproject/MainActivity.java
@@ -0,0 +1,13 @@
+package org.esiea.chamoinri_ifuja_firmin_jacques_olivier.androidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
