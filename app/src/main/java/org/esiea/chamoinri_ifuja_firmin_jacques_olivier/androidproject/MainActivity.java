8869c0dfc99f2d05f24c01de4f5f73d87fb72065
ifuja <ifuja.chamoinri@gmail.com>
2018-05-28 15:58:14 +0200

    Android Project 3

--- app/src/main/java/org/esiea/chamoinri_ifuja_firmin_jacques_olivier/androidproject/MainActivity.java
+++ app/src/main/java/org/esiea/chamoinri_ifuja_firmin_jacques_olivier/androidproject/MainActivity.java
@@ -1,7 +1,9 @@
 package org.esiea.chamoinri_ifuja_firmin_jacques_olivier.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void page1 (View view) {
+        startActivity(new Intent(this, page_2.class));
+    }
+    public class page_2 extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.page_2);
+        }
+
+        public void page2 (View view){
+            startActivity(new Intent(this, MainActivity.class));
+        }
     }
 }
